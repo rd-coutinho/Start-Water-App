4ab70b699aa54f269bcec942ea2399a6274f8720
rd-coutinho <56792861+rd-coutinho@users.noreply.github.com>
2019-10-20 11:24:28 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,32 @@
+# Variáveis e cálculos utilizados no aplicativo
+# Meses
+ano1 = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro',
+        'novembro', 'dezembro']
+
+ano2 = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro',
+        'novembro', 'dezembro']
+
+# Volume de chuva mensal (m3)
+
+# Chuva média mensal (mm)
+
+# Área (m2)
+
+# Demanda mensal (m3)
+
+# Volume da cisterna (m3)
+
+# Nível do reservatório (antes)
+
+# Nível do reservatório (depois)
+
+# Uso de água potável
+
+# Métodos para dimensionamento 
+# Método prático Azevedo Neto
+V = 0.042 * 'P' * 'A' * 'T'
+
+# Método Rippl
+
+# Para um exemplo hipotético usando P = 1477, A = 900, T = 3, no método Azevedo Neto V = 170 m3.
+# No método Rippl V = 409 m3.
